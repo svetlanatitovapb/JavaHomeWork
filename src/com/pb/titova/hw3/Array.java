8e631a569e6bef8b994fc944f15b4697ae4229e0
Svetlana Titova <svetlana.titova@privatbank.ua>
2021-10-23 18:30:28 +0300

    init commit

---
+++ src/com/pb/titova/hw3/Array.java
@@ -0,0 +1,44 @@
+package com.pb.titova.hw3;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args){
+        int[] array = new int[10];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input ten whole numbers:");
+        for (int i = 0; i < array.length; i++ ){
+            array[i] = in.nextInt();
+        }
+        for (int i = 0; i < array.length; i++ ){
+            System.out.println(array[i]);
+        }
+        int summa = 0;
+        for (int i =0; i < array.length; i++ ){
+            summa += array[i];
+        }
+        System.out.println("Sum: "+ summa);
+        int colo =0;
+        for (int i = 0; i < array.length; i++ ){
+            if (array[i]>=0){
+                colo++;
+            }
+        }
+        System.out.println("amount of positive numbers: "+colo );
+        for (int i =0; i < array.length-1; i++){
+            for (int j =0; j < array.length-1; j++){
+               if (array[j]>array[j+1]){
+                   int left = array[j];
+                   array[j] = array[j+1];
+                   array[j+1] = left;
+               }
+            }
+
+        }
+        for (int i = 0; i < array.length; i++ ){
+            System.out.println(array[i]);
+        }
+
+    }
+
+}
